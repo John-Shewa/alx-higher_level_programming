f23af5c08ad06ef0d718fbd342e797b9e7e4b32a
John-Shewa <eldi2002@gmail.com>
2023-03-07 10:53:17 -0800

    commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for alpha_letters in range(97, 123):
-    if alpha_letters == e or alpha_letters == q:
-        continue
-    print("{:c}".format(alpha_letters), end=" ")
+for c in range(ord('a'), ord('z') + 1):
+    if c != ord('e') and c != ord('q'):
+        print("{:c}".format(c), end="")
